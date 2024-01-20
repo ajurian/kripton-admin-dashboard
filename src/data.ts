6a2e8a6ca8fd162fe290244082b960137528fec9
AdolfUrian <adolfjamesurian@gmail.com>
2024-01-20 15:52:44 +0800

    Fixed type name

--- src/data.ts
+++ src/data.ts
@@ -1,12 +1,12 @@
 import profilePicture from "@/assets/images/profile-picture.png";
 
-export const user: User = {
+export const user: UserData = {
     username: "Johndoe",
     role: "Super Admin",
     photoUrl: profilePicture.src,
 };
 
-export const wallets: Wallet[] = [
+export const wallets: WalletData[] = [
     { cardNumber: "444 221 224 424", name: "Main Wallet", balance: 45500.12 },
     { cardNumber: "444 221 224 424", name: "XYZ Wallet", balance: 250.5 },
     { cardNumber: "444 221 224 424", name: "Main Wallet", balance: 45500.12 },
